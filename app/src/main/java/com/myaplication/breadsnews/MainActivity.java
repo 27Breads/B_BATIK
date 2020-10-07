c6b815a160eab965510ee635b2b033406357c6a8
chandra171 <quizenyui171@gmail.com>
2020-10-07 19:47:46 +0700

    Menambah About, Menu, dan list batik

--- app/src/main/java/com/myaplication/breadsnews/MainActivity.java
+++ app/src/main/java/com/myaplication/breadsnews/MainActivity.java
@@ -2,13 +2,43 @@ package com.myaplication.breadsnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialCardView mPengetahuan;
+    private MaterialCardView mProfil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        mPengetahuan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ListBatikActivity.class));
+            }
+        });
+
+        mProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+            }
+        });
     }
+
+    private void initView() {
+        mPengetahuan = (MaterialCardView) findViewById(R.id.cv_pengetahuan);
+        mProfil = (MaterialCardView) findViewById(R.id.cv_profil);
+    }
+
 }

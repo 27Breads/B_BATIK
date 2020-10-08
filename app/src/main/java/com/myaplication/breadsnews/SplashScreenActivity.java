e329d4111b7fab23807ce8141295a616cb4dfefe
27Breads <airfande@emailme.my.id>
2020-10-08 11:33:21 +0700

    Hapus Splash Screen kedua

--- app/src/main/java/com/myaplication/breadsnews/SplashScreenActivity.java
+++ app/src/main/java/com/myaplication/breadsnews/SplashScreenActivity.java
@@ -24,7 +24,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             @Override
             public void run() {
                 finishAffinity();
-                startActivity(new Intent(getApplicationContext(), SplashActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         }, 4000);
     }
